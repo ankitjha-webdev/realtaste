ac9c4b4874343edfe1d63dedb9231173a45ce2a1
ankitkumarcse91 <ankitkumarcse91@gmail.com>
2023-12-20 16:41:02 +0000

    new commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="logo react">
+        {/* <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a> */}
+        {/* <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+      </div>
+      <h1>Ankit + Deva</h1>
+      <div className="card">
+        {/* <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button> */}
+        <p>
+          Deva tum chutiya hai bsdkwala hai, gali dene ka bhout man krta hai pr nhi dene ka man krta hai phir bhi phir se dene ka man krta hai 
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
